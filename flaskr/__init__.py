677ba780ce98236e132aecf7dcfe5abc5421e854
Rafael Takeguma <rafael08.0807@gmail.com>
2024-04-03 22:33:52 -0300

    cria pasta flaskr e add __init__.py

---
+++ flaskr/__init__.py
@@ -0,0 +1,34 @@
+import os
+
+from flask import Flask
+
+"""
+Uma aplicação Flask é uma instância da classe Flask. Todas as configurações e URLs
+serão registradas com essa classe.
+Abaixo criamos uma instância 'app' da classe Flask dentro de uma função 'create_app()' 
+esse é chamada de application factory, qualquer configuração vai dentro dessa função.
+
+Uso: flask --app flaskr run --debug
+"""
+def create_app(test_config=None):
+    # Instanciando um objeto 'app' da classe Flask
+    app = Flask(__name__, instance_relative_config=True)
+    
+    # Definindo o caminho do Banco de Dados SQLite
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite') 
+    )
+
+    # Criando uma pasta de instância, se ainda não existir
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    # Criando uma primeira rota de teste
+    @app.route('/hello')
+    def hello():
+        return 'Hello 21!'
+    
+    return app
