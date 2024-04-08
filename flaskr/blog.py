7121e501e372f5eb8fed631c3ee76f35733d37bc
Rafael Takeguma <rafael08.0807@gmail.com>
2024-04-08 10:16:56 -0300

    implementa rota index

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -7,3 +7,17 @@ from flaskr.auth import login_required
 from flaskr.db import get_db
 
 blueprint = Blueprint('blog', __name__)
+
+# Rota index
+@blueprint.route('/')
+def index():
+    db = get_db()
+
+    # Selecionando todos os posts
+    posts = db.execute(
+        "SELECT p.id, title, body, created, author_id, username"
+        "   FROM post p JOIN user u ON(p.author_id = u.id)"
+        "   ORDER BY created DESC"
+    ).fetchall
+
+    return render_template('blog/index.html', posts=posts)
