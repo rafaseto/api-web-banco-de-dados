52d3456d51c12fd4edba9f5d65c81ec579e07f83
Rafael Takeguma <rafael08.0807@gmail.com>
2024-04-08 08:17:49 -0300

    implementa rota register em auth.py

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -4,7 +4,37 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
+from werkzeug.security import check_password_hash, generate_password_hash
+
 from flaskr.db import get_db
 
 # Criando uma blueprint chamada auth 
 blueprint = Blueprint('auth', __name__, url_prefix='/auth')
+
+@blueprint.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Usuário é obrigatório.'
+        elif not password:
+            error = 'Senha é obrigatória.'
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password))
+                )
+            except db.IntegrityError:
+                error = f"Usuário {username} já existe."
+            else:
+                return redirect(url_for("auth.login"))
+            
+        flash(error)
+
+    return render_template('auth/register.html')
